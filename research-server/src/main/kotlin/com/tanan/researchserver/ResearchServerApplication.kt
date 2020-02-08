ae62f8a801d5780553f0374dc3dee930eccca636
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-02-08 21:38:28 +0900

    first commit

---
+++ research-server/src/main/kotlin/com/tanan/researchserver/ResearchServerApplication.kt
@@ -0,0 +1,11 @@
+package com.tanan.researchserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResearchServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResearchServerApplication>(*args)
+}
---
+++ research-server/src/test/kotlin/com/tanan/researchserver/ResearchServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tanan.researchserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResearchServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
