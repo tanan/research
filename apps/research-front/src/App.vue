b9d1c44fcbf916932a8765a6ebe79f99a33dcbbb
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-02-22 19:24:30 +0900

    display new article cards

--- apps/research-front/src/App.vue
+++ apps/research-front/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="#fff" dense flat>
       <v-toolbar-title>Research</v-toolbar-title>
-      <v-spacer></v-spacer>
     </v-app-bar>
     <v-content>
       <NotificationContainer />
       <router-view></router-view>
     </v-content>
     <v-footer
-      color="primary lighten-1"
+      color="#fff"
       padless
     >
       <v-row
         justify="center"
         no-gutters
       >
-        <!-- <v-btn
-          v-for="link in links"
-          :key="`${link.label}-header-link`"
-          color="white"
-          text
-          rounded
-          class="my-2"
-          :to="link.url"
-        >
-          {{ link.label }}
-        </v-btn> -->
         <v-col
           class="primary lighten-2 py-4 text-center white--text"
           cols="12"
--- apps/research-front/src/views/ResearchTop.vue
+++ apps/research-front/src/views/ResearchTop.vue
@@ -2,10 +2,20 @@
   <div>
     <v-container>
       <v-row>
-        <v-col v-for="i of articleCount" :key="i">
-          <div :id="`form-${i}`">
-            <p>コンテンツを書く</p>
+        <v-col md="8">
+          <div class="new-article">
+            <h2>新着記事</h2>
           </div>
+          <v-row>
+            <v-col class="pt-6 pr-8" md="6" v-for="(article, index) in articles" :key="index">
+              <v-card min-height="340px">
+                <v-img class="white--text align-end" height="180px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                  <v-card-title>{{ article.title }}</v-card-title>
+                </v-img>
+                <v-card-text>{{ article.content }}</v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -25,7 +35,24 @@ export default {
   },
   data () {
     return {
-      articleCount: 2
+      articles: [
+        {
+          title: "タイトル1",
+          content: "コンテンツが流れています"
+        },
+        {
+          title: "タイトル2",
+          content: "コンテンツが流れています"
+        },
+        {
+          title: "タイトル1",
+          content: "コンテンツが流れています"
+        },
+        {
+          title: "タイトル2",
+          content: "コンテンツが流れています"
+        }
+      ]
     }
   },
   methods: {
@@ -36,5 +63,10 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.new-article {
+  h2 {
+    font-size: 18px;
+  }
+}
 </style>
