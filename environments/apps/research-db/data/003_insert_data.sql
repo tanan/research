b174c8cbdaf0ad25df5423a3ea5f99a249127d33
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-04-04 09:28:34 +0900

    data: 修正

--- environments/apps/research-db/data/003_insert_data.sql
+++ environments/apps/research-db/data/003_insert_data.sql
@@ -4,4 +4,5 @@ insert into "research"."user" values
 ('08dc687d-fbbf-4644-82d9-203e62d34858', 'dolphin@example.com', '55566188e549e72ba5986274a15c24eb85490a73edb28400f46406da24e2b6b5', 'Dolphin');
 
 insert into "research"."article" values
-('5qopcYbpSTaV7a1AIqzkSu', 'article test', 'これはarticleのDescriptionです', 'editor', '2020-02-22 12:23:34', 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png');
+('5qopcYbpSTaV7a1AIqzkSu', 'article test', 'これはarticleのDescriptionです', 'editor', '2020-02-22 12:23:34', 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'),
+('5qopcYbpSTaV7a1AIqzkS1', 'article test2', 'これはarticleのDescriptionです2', 'editor', '2020-02-21 12:23:34', 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png');
