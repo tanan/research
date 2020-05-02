e60556a250a6b2414c5bf67c35532871c6a7e5db
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-05-02 18:54:40 +0900

    delete vue

--- apps/research-front-vue/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tag=latest
-
-docker stop research-front
-docker rm -v research-front
-docker run --network research -d -p 80:80 -v $(pwd)/environment/conf.d:/etc/nginx/conf.d:ro --name research-front research-front:${tag}
--- apps/research-front-vue/src/repositories/RepositoryFactory.js
+++
@@ -1,9 +0,0 @@
-import ArticleRepository from "./ArticleRepository"
-
-const repositories = {
-  articles: ArticleRepository
-}
-
-export const RepositoryFactory = {
-  get: name => repositories[name]
-}
