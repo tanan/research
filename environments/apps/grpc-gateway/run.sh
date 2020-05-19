efe54d9c9b7b96fe0bd5642822239b66da185023
Toshifumi Anan <toshifumi.anan@uzabase.com>
2020-05-19 18:49:01 +0900

    fix data

--- environments/apps/grpc-gateway/run.sh
+++ environments/apps/grpc-gateway/run.sh
@@ -9,4 +9,4 @@ count=$(docker network ls | grep ${network} | wc -l)
 [[ ${count} -ne 1 ]] && docker network create ${network}
 
 docker stop ${name} && docker rm ${name}
-docker run --network ${network} --name ${name} -d -p 5000:5000 ${registry}/${name}:latest
+docker run --network ${network} --name ${name} -d -p 5000:5000 ${registry}/grpc-gateway:latest
